dee04bccfe5f45254d0518d81b76efee93b3d171
Lyndon Dwight Torrado <lyndondwighttorrado08052007@gmail.com>
2026-05-07 09:40:36 +0800

    asd

--- js/navbar.js
+++ js/navbar.js
@@ -1,12 +1,74 @@
-const menuToggle =
-    document.querySelector(".menu-toggle");
+window.addEventListener("scroll", () => {
 
-const navMenu =
-    document.querySelector(".nav-menu");
+    const navbar = document.querySelector(".navbar");
+
+    if(window.scrollY > 50){
+        navbar.classList.add("scrolled");
+    }
+    else{
+        navbar.classList.remove("scrolled");
+    }
+
+});
 
 
-menuToggle.addEventListener("click", () => {
+window.addEventListener("DOMContentLoaded", () => {
 
-    navMenu.classList.toggle("active");
+    const currentPage = window.location.pathname.split("/").pop();
+
+    const navLinks = document.querySelectorAll(".nav-link");
+
+    navLinks.forEach(link => {
+
+        const linkPage = link.getAttribute("href");
+
+        if(linkPage === currentPage){
+            link.classList.add("active");
+        }
+
+    });
+
+});
+
+window.addEventListener("click", (e) => {
+
+    const toggle = document.querySelector(".menu-toggle");
+    const menu = document.querySelector(".nav-menu");
+
+    if(e.target === toggle){
+        menu.classList.toggle("show");
+    }
+
+});
+
+window.addEventListener("scroll", () => {
+
+    const navbar = document.querySelector(".navbar");
+
+    if(window.scrollY > 50){
+        navbar.classList.add("scrolled");
+    }
+    else{
+        navbar.classList.remove("scrolled");
+    }
+
+});
+
+
+window.addEventListener("DOMContentLoaded", () => {
+
+    const currentPage = window.location.pathname.split("/").pop();
+
+    const navLinks = document.querySelectorAll(".nav-link");
+
+    navLinks.forEach(link => {
+
+        const linkPage = link.getAttribute("href");
+
+        if(linkPage === currentPage){
+            link.classList.add("active");
+        }
+
+    });
 
 });
