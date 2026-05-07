0fc8d545e83c602b7703ccb1cd1e6e4199066fbf
Lyndon Dwight Torrado <lyndondwighttorrado08052007@gmail.com>
2026-05-07 10:20:30 +0800

    index responsive

--- js/navbar.js
+++ js/navbar.js
@@ -1,74 +1,50 @@
-window.addEventListener("scroll", () => {
+const navbar = document.querySelector(".navbar");
 
-    const navbar = document.querySelector(".navbar");
+const menuToggle =
+    document.querySelector(".menu-toggle");
 
-    if(window.scrollY > 50){
-        navbar.classList.add("scrolled");
-    }
-    else{
-        navbar.classList.remove("scrolled");
-    }
+const navMenu =
+    document.querySelector(".nav-menu");
 
-});
+const navLinks =
+    document.querySelectorAll(".nav-link");
 
-
-window.addEventListener("DOMContentLoaded", () => {
-
-    const currentPage = window.location.pathname.split("/").pop();
-
-    const navLinks = document.querySelectorAll(".nav-link");
-
-    navLinks.forEach(link => {
-
-        const linkPage = link.getAttribute("href");
-
-        if(linkPage === currentPage){
-            link.classList.add("active");
-        }
-
-    });
-
-});
-
-window.addEventListener("click", (e) => {
-
-    const toggle = document.querySelector(".menu-toggle");
-    const menu = document.querySelector(".nav-menu");
-
-    if(e.target === toggle){
-        menu.classList.toggle("show");
-    }
-
-});
 
 window.addEventListener("scroll", () => {
 
-    const navbar = document.querySelector(".navbar");
-
     if(window.scrollY > 50){
+
         navbar.classList.add("scrolled");
+
     }
     else{
+
         navbar.classList.remove("scrolled");
+
     }
 
 });
 
 
-window.addEventListener("DOMContentLoaded", () => {
+const currentPage =
+    window.location.pathname.split("/").pop();
 
-    const currentPage = window.location.pathname.split("/").pop();
+navLinks.forEach(link => {
 
-    const navLinks = document.querySelectorAll(".nav-link");
+    const linkPage =
+        link.getAttribute("href");
 
-    navLinks.forEach(link => {
+    if(linkPage === currentPage){
 
-        const linkPage = link.getAttribute("href");
+        link.classList.add("active");
 
-        if(linkPage === currentPage){
-            link.classList.add("active");
-        }
+    }
 
-    });
+});
+
+
+menuToggle.addEventListener("click", () => {
+
+    navMenu.classList.toggle("active");
 
 });
